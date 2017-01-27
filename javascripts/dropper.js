55e022a9b074ea171c8268c6c50d23d7683d3736
kevwang424 <kev.wang424@gmail.com>
2017-01-27 08:57:16 -0500

    board is reacting to dropper

--- javascripts/dropper.js
+++ javascripts/dropper.js
@@ -7,16 +7,16 @@ class Dropper {
 
   constructor(){
     this.dropRow = ["H", "H", "H", "H", "H", "H", "H"]
-    this.token = "P"
+    // this.token = "P"
   }
 
-  tokenPlace(this.dropRow){
-      this.dropRow[0] = this.token
-  }
+  // tokenPlace(this.dropRow){
+  //     this.dropRow[0] = this.token
+  // }
 
   render(){
 
-    tokenPlace(this.dropRow)
+    // tokenPlace(this.dropRow)
 
     this.dropRow.forEach(function(hiddenRow, i){
       $('#board').append(`<div class = "hidden-column" id = "hidden-column-${i}">${hiddenRow}</div>`)
